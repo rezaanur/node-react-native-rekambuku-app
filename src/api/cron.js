859fdcf6390c0d8563d8a17ce5028978be462eb4
rezaanur <rezanur2610@gmail.com>
2025-12-23 20:57:35 +0700

    Fix: Implement standard Vercel Serverless Cron newewst

--- src/api/cron.js
+++ src/api/cron.js
@@ -1,11 +1,11 @@
-// backend/api/cron.js
-export default function handler(req, res) {
-  // Cek keamanan dari Vercel
-  const authHeader = req.headers["authorization"];
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
+// // backend/api/cron.js
+// export default function handler(req, res) {
+//   // Cek keamanan dari Vercel
+//   const authHeader = req.headers["authorization"];
+//   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+//     return res.status(401).json({ message: "Unauthorized" });
+//   }
 
-  console.log("Cron Job Berhasil dijalankan!");
-  return res.status(200).json({ success: true, message: "Server is awake!" });
-}
+//   console.log("Cron Job Berhasil dijalankan!");
+//   return res.status(200).json({ success: true, message: "Server is awake!" });
+// }
