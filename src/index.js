4702f5ca311c3c05a9f5f52d68c69c89b0d82bef
rezaanur <rezanur2610@gmail.com>
2025-12-23 14:02:44 +0700

    Update index.js for Vercel deployment

--- src/index.js
+++ src/index.js
@@ -13,12 +13,23 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
+// Jalankan koneksi database di luar listen agar lebih stabil di serverless
+connectDB();
+
 // Endpoint Authentication
 app.use("/api/auth", authRoutes);
 
 app.use("/api/books", bookRoutes);
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`server is running on port ${PORT}`);
-  connectDB();
-});
+// app.listen(PORT, "0.0.0.0", () => {
+//   console.log(`server is running on port ${PORT}`);
+//   connectDB();
+// });
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+export default app;
