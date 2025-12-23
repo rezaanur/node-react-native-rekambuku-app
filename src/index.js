8cddbb18807c0ffbdf0d8a3281534e4707bf04cd
rezaanur <rezanur2610@gmail.com>
2025-12-23 23:16:14 +0700

    Fix: Error Charracter A pada saat sign up

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 
 import { connectDB } from "./lib/db.js";
-import { cronJobHandler } from "./lib/cronHandler.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -23,12 +22,35 @@ app.use("/api/auth", authRoutes);
 // Endpoint Books
 app.use("/api/books", bookRoutes);
 
-// Cron Job Endpoint
-app.get("/api/cron", cronJobHandler);
 // app.listen(PORT, "0.0.0.0", () => {
 //   console.log(`server is running on port ${PORT}`);
 //   connectDB();
 // });
+
+// Cron Job Endpoint
+// Cron job berfungsi untuk menjaga server agar tetap aktif di platform seperti Vercel
+app.get("/api/cron", (req, res) => {
+  try {
+    const authHeader = req.headers["authorization"];
+
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+      console.error("Gagal: Secret tidak cocok!");
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+
+    console.log("Cron Job manual run triggered! Server tetap aktif.");
+
+    res.status(200).json({
+      success: true,
+      message: "Keep-alive successful",
+      timestamp: new Date().toISOString(),
+    });
+  } catch (error) {
+    console.error("Cron Crash Error:", error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+// End cron
 
 // Root endpoint (opsional agar saat buka domain tidak muncul 'Cannot GET /')
 app.get("/", (req, res) => {
